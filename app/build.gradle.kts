5602aa86072e47b988f1f7d6282f127d79c8345a
Fernando <ferlagod@oniros.eu>
2025-02-28 21:39:15 +0100

    Added about info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "dam.pmdm.spyrothedragon"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
